e973b4dbcfb3e6a9343062d8ddb68c065d832cc7
Muhtasin Jawad <mutasin.jawad@g.bracu.ac.bd>
2025-02-15 13:43:33 +0600

    2. Task Mapping complete and starting client side interface

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -4,5 +4,19 @@ pragma solidity ^0.8.16;
 contract ToDoList {
     uint public taskCount;
 
-    
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() {
+        createTask("This is your to do List!");
+    }
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
